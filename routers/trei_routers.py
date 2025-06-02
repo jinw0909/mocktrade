c8af54ce985afa0c280c1b10bc9cd737dc49cd5a
Jinwoo Chang <83801879+jinw0909@users.noreply.github.com>
2025-06-02 19:03:17 +0900

    update router

--- routers/trei_routers.py
+++ routers/trei_routers.py
@@ -124,7 +124,7 @@ async def api_select(user_no: str, symbol: str, margin_type: int, leverage: int,
     try:
         data=mysql.get_check_user(user_no)
         if len(data)>0  :
-          
+
             user_id=data['id'].iloc[0]
             mysql.buy_market_order(user_id,symbol,margin_type,leverage,usdt,amount,tp,sl)
             mysql.insert_trade_log(user_id, symbol, 'market', margin_type, 'buy', mysql.price1, usdt, amount, leverage, tp,sl,mysql.return_dict_data['reCode'],mysql.return_dict_data['message'])
